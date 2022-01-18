e341a9e30be73bfea2c1aaebcfd5028db466f6b5
Nadege ALAVOINE <anadege@e2r5p13.clusters.42paris.fr>
2022-01-18 12:32:13 +0100

    repo made

---
+++ containers/vector.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 12:21:45 by anadege           #+#    #+#             */
+/*   Updated: 2022/01/18 12:26:24 by anadege          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+namespace ft
+{
+    template <class T, class Alloc = allocator<T> >
+    class vector
+    {
+        
+    };
+};
+
+#endif
