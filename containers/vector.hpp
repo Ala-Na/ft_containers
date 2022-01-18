0ac03e7bfe8d71cd0b331793122951d4cf21f678
Nadege ALAVOINE <anadege@e2r5p13.clusters.42paris.fr>
2022-01-18 17:31:29 +0100

    lolilol

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -6,16 +6,22 @@
 /*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 12:21:45 by anadege           #+#    #+#             */
-/*   Updated: 2022/01/18 17:14:25 by anadege          ###   ########.fr       */
+/*   Updated: 2022/01/18 17:30:27 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
+//Must use std::allocator
+//Must implement system of iterator if present in container
+//STD lib is authorized
+//STL containers are forbidden
+
+
 namespace ft
 {
-    template <class T, class Alloc = allocator<T> >
+    template <class T, class Alloc = std::allocator<T> >
     class vector
     {
         public:
@@ -28,7 +34,7 @@ namespace ft
             typedef const T&    const_reference;
             typedef size_t      size_type;
             typedef ptdrdiff_t  difference_type;
-                
+
 
             explicit vector (const allocator_type& alloc = allocator_type())
     };
