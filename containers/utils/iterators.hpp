f9c2f2d6cb79fe1640e34dfd2853af0200cf279f
anadege <anadege@student.42.fr>
2022-01-20 21:22:04 +0100

    c tro nul com sujet

--- containers/utils/iterators.hpp
+++ containers/utils/iterators.hpp
@@ -6,7 +6,7 @@
 /*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 15:57:47 by anadege           #+#    #+#             */
-/*   Updated: 2022/01/19 16:24:39 by anadege          ###   ########.fr       */
+/*   Updated: 2022/01/19 16:57:53 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,32 @@ namespace ft
 	struct bidirectionnal_iterator_tag : public forward_iterator_tag {};
 	struct random_access_iterator_tag : public bidirectionnal_iterator_tag {};
 
+	template <class Iterator>
+	struct iterator_traits {
+		typedef typename Iterator::iterator_category	iterator_category;
+		typedef typename Iterator::value_type			value_type;
+		typedef typename Iterator::difference_type		difference_type;
+		typedef typename Iterator::pointer				pointer;
+		typedef typename Iterator::reference			reference;
+	};
 
+	template <class T>
+	struct iterator_traits<T*> {
+		typedef ft::random_access_iterator_tag	iterator_category;
+		typedef T							value_type;
+		typedef std::ptrdiff_t				difference_type;
+		typedef T							*pointer;
+		typedef T							&reference;
+	};
 
-
+	template <class T>
+	struct iterator_traits<const T*> {
+		typedef ft::random_access_iterator_tag	iterator_category;
+		typedef T							value_type;
+		typedef std::ptrdiff_t				difference_type;
+		typedef T							*pointer;
+		typedef T							&reference;
+	};
 }
 
 #endif
