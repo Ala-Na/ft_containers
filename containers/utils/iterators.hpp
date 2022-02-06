f8ca9da96f59b110fde6dc5bc0207be7c5aa87c2
anadege <ilmefautuneadresse@gmail.com>
2022-02-06 19:18:19 +0100

    implementation equal comparison

--- containers/utils/iterators.hpp
+++ containers/utils/iterators.hpp
@@ -6,7 +6,7 @@
 /*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 15:57:47 by anadege           #+#    #+#             */
-/*   Updated: 2022/02/06 17:55:46 by anadege          ###   ########.fr       */
+/*   Updated: 2022/02/06 19:18:01 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -255,7 +255,32 @@ namespace ft
 	template <class T>
 	struct is_integral<wchar_t> : public true_type {};
 
-	
+	// equal compare implementation
+	template <class InputIt1, class InputIt2>
+	bool	equal (InputIt1 first1, InputIt1 last1, InputIt2  first2)
+	{
+		for (; first1 != last1; ++first1, ++first2)
+		{
+			if (!(*first1 == *first2))
+				return false;
+		}
+		return true;
+	};
+
+	template <class InputIt1, class InputIt2, class BinaryPredicate>
+	bool	equal (InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p);
+	{
+		for (; first1 != last1; ++first1, ++first2) {
+			if (!p(*first1, *first2)) {
+				return false;
+			}
+		}
+		return true;
+	};
+
+	// lexicographical_compare implementation
+	template <class InputIt1, class InputIt2>
+	bool	lexicographical_compare (InputIt1 )
 
 };
 
